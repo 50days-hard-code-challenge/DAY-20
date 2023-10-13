c02a2b6d2c2b2681df2e16d7761fc1134bcb1ce8
Madhav Sharma <116294690+PoSeiDonXMs@users.noreply.github.com>
2023-10-13 23:13:32 +0530

    Create Madhav2(Union and Intersection of two sorted arrays).cpp

---
+++ Madhav2(Union and Intersection of two sorted arrays).cpp
@@ -0,0 +1,40 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<int> Unionarray(int arr1[], int arr2[], int n, int m)
+{
+	set<int> s;
+	// Remove the duplicates from arr1[]
+	for (int i = 0; i < n; i++) {
+		s.insert(arr1[i]);
+	}
+
+
+	for (int i = 0; i < m; i++) {
+		s.insert(arr2[i]);
+	}
+
+	// Loading set to vector
+	vector<int> vec(s.begin(), s.end());
+
+	return vec;
+}
+
+
+int main()
+{
+	int arr1[] = { 1, 2, 2, 2, 3 };
+	int arr2[] = { 2, 3, 3, 4, 5, 5 };
+	int n = sizeof(arr1) / sizeof(arr1[0]);
+	int m = sizeof(arr2) / sizeof(arr2[0]);
+
+
+	vector<int> uni = Unionarray(arr1, arr2, n, m);
+	for (int i : uni) {
+		cout << i << " ";
+	}
+
+	return 0;
+}
